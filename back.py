22b29fabe96653a28b0c595d1e373ce8a27531bb
tristan_retali <tristan.retali@gmail.com>
2023-10-20 09:52:25 +0200

    feat: :sparkles: Added the first tool who extract id recipe and recipe name

--- back.py
+++ back.py
@@ -1,43 +1,79 @@
 import config
 from langchain.prompts import PromptTemplate
 from langchain.llms.openai import OpenAI
+from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
+from langchain.agents import Tool, initialize_agent, AgentType
+from langchain.tools import BaseTool
+from pydantic import BaseModel, Field
+from typing import Optional, Type
 import requests
 import pprint
+import json
 
 
 # 100 request per day and is free to create an account
-spoonacular_api_key = config.FOOD_API_KEY
+SPOONACULAR_API_KEY = config.FOOD_API_KEY
 
 
-FIND_RELEVANT_INFORMATIONS = """ You are a chef and your role is to find the relevant informations
-in the following content: ### {user_input} ###
+BASIC_URL = "https://api.spoonacular.com/recipes/"
 
-I will list you the different relevant informations you should extract:
 
-1. The ingredient and if there is no food put me None as a value
-Exemple: 'ingredient': "artichoke", "apple"
-##
-
-You should return the information in json format
-
-"""
-
-LLM = OpenAI(
+LLM = ChatOpenAI(
     openai_api_key=config.OPENAI_API_KEY,
-    temperature=0.0
+    temperature=0.0,
+    max_tokens=512
 )
 
-PROMPT_TEMPLATE = PromptTemplate(
-    template=FIND_RELEVANT_INFORMATIONS,
-    input_variables=["user_input"]
-)
 
-LLM_CHAIN = LLMChain(
-    llm=LLM,
-    prompt=PROMPT_TEMPLATE,
-    verbose=True
-)
+def find_by_ingredients(ingredients):
+    url = f"{BASIC_URL}findByIngredients?apiKey={SPOONACULAR_API_KEY}&ingredients={ingredients}&number=3"
+    response = requests.get(url)
+    response_json = response.json()
+    return response_json
+
+
+class FindByIngredientsCheckInput(BaseModel):
+    """ Input for finding recipe with ingredients"""
+
+    ingredients: str = Field(...,
+                             ingredients="The ingredients who will be use for the recipe")
+
+
+class FindByIngredientsTool(BaseTool):
+    name = "find_by_ingredients"
+    description = "Useful when you need to find recipes based on ingredients."
+    return_direct = True
+
+    def _run(self, ingredients: str):
+        response = find_by_ingredients(ingredients)
+        recipes = []
+        for recipe in response:
+            recipe_info = {
+                "id": recipe["id"],
+                "title": recipe["title"]
+            }
+            recipes.append(recipe_info)
+        return recipes
+
+    def _arun(self, ingredients: str):
+        raise NotImplementedError("This tool does not support async")
+
+    args_schema: Optional[Type[BaseModel]
+                          ] = FindByIngredientsCheckInput
+
+
+tools = [FindByIngredientsTool()]
+
+agent = initialize_agent(tools=tools, llm=LLM,
+                         agent=AgentType.OPENAI_FUNCTIONS, verbose=True)
 
 if __name__ == "__main__":
-    print(LLM_CHAIN("I would like to find recipe with pasta and chicken"))
+    query = "I would like to cook something with pasta and chicken"
+    # resultat = LLM_CHAIN(query)["text"]
+    # print(resultat)
+    # # convert in dict
+    # find_recipes(json.loads(resultat))
+    # convert in string
+    # find_recipes(json.dumps(resultat))
+    res = agent.run(query)
