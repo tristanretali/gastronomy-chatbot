37a253cb4449bf5641a31a08fad53dd19f9315c8
tristan_retali <tristan.retali@gmail.com>
2024-03-21 09:51:23 +0100

    [FEATURE] Ajout de Tailwind et commencement du design

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("polls/", include("backend_app.urls")),
+    path("", include("backend_app.urls")),
     path('admin/', admin.site.urls),
 
 ]
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Chatbot from "./chatbot";
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    return (
+    <div className="min-w-full min-h-screen bg-neutral-600 text-center">
+        <main className="container bg-green-800 min-h-screen flex flex-col justify-center items-center">
+            <Chatbot/>
+        </main>
     </div>
   );
 }
