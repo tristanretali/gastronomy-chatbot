2ad0d1d786dfe8b435e707ac28a1d41f63b288be
tristan_retali <tristan.retali@gmail.com>
2024-04-06 01:15:43 +0200

    [LAYOUT] Added buttons on header

--- frontend/src/header.js
+++ frontend/src/header.js
@@ -3,8 +3,12 @@ import React from "react";
 export default class Header extends React.Component {
   render() {
     return (
-      <div className="bg-indigo-300 min-h-12 md:rounded-t-[0.35rem]">
-        <h1> Header</h1>
+      <div className="bg-gradient-to-r from-indigo-400 to-indigo-300 min-h-12 flex flex-row items-center justify-between md:rounded-t-[0.35rem]">
+        <h1 className="italic text-xl font-bold">Gastronomy Chatbot</h1>
+        <div className="w-16 flex justify-around">
+          <i className="fi fi-rr-refresh"></i>
+          <i className="fi fi-br-cross"></i>
+        </div>
       </div>
     );
   }
