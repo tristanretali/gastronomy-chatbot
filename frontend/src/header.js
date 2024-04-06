2eae99ebb32bfe27819ddc0a885dc2dd364cf669
tristan_retali <tristan.retali@gmail.com>
2024-04-06 16:08:19 +0200

    [LAYOUT] Fix the header icons size, hover effect and general background color

--- frontend/src/header.js
+++ frontend/src/header.js
@@ -3,14 +3,14 @@ import React from "react";
 export default class Header extends React.Component {
   render() {
     return (
-      <div className="bg-gradient-to-r from-indigo-400 to-indigo-300 min-h-12 flex flex-row items-center justify-between md:rounded-t-[0.35rem]">
-        <div className="flex justify-around w-header text-xl">
+      <div className="bg-gradient-to-r from-indigo-400 to-indigo-300 min-h-12 flex flex-row items-center justify-between text-xl md:rounded-t-[0.35rem]">
+        <div className="flex justify-around w-header">
           <i className="fi fi-rr-chatbot-speech-bubble text-2xl"></i>
           <h1 className="italic font-bold">Gastronomy Chatbot</h1>
         </div>
-        <div className="w-16 flex justify-around">
-          <i className="fi fi-rr-refresh"></i>
-          <i className="fi fi-br-cross"></i>
+        <div className="w-20 flex justify-around">
+          <i className="fi fi-rr-refresh hover:cursor-pointer"></i>
+          <i className="fi fi-br-cross hover:cursor-pointer"></i>
         </div>
       </div>
     );
