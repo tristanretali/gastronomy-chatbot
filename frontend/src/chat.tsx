0cb87f82f86f4a5bd2504975db6d6acef0cf1151
tristan_retali <tristan.retali@gmail.com>
2024-04-06 17:09:55 +0200

    [FEATURE] Added chat variables

--- frontend/src/chat.tsx
+++ frontend/src/chat.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 
-export default class Chat extends React.Component {
+interface Props {
+  chatbotMessage: string[];
+  userMessage: string[];
+}
+
+export default class Chat extends React.Component<Props> {
   render() {
     return (
       <div className="bg-red-500 w-11/12 ml-[4%] mt-2">
--- frontend/src/chatbot.tsx
+++ frontend/src/chatbot.tsx
@@ -2,13 +2,27 @@ import React from "react";
 import Header from "./header";
 import Chat from "./chat";
 import UserInput from "./userInput";
-export default class Chatbot extends React.Component {
+interface State {
+  chatbotMessage: string[];
+  userMessage: string[];
+}
+export default class Chatbot extends React.Component<{}, State> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      chatbotMessage: [],
+      userMessage: [],
+    };
+  }
+
   render() {
+    const chatbotMessage: string[] = this.state.chatbotMessage;
+    const userMessage: string[] = this.state.userMessage;
     return (
       <div className="w-full flex flex-col min-h-screen justify-between pb-6 text-white md:min-h-desktop md:border-solid md:border-2 md:border-cyan-500 md:rounded-lg md:w-11/12 md:mt-6 ">
         <div>
           <Header />
-          <Chat />
+          <Chat chatbotMessage={chatbotMessage} userMessage={userMessage} />
         </div>
         <UserInput />
       </div>
